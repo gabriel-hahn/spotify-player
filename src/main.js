076bc976a5c9af65e2ce41781c4315e54ff1173c
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-08-28 20:45:04 -0300

    AlbumInfo unit test and methods

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Guns N Roses');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('3I9Z1nDCL4E0cP62flcbI5');
+const albumInfo = document.getElementById('album-info');
+
 albums.then(data => renderAlbums(data.albums.items, albumList));
+album.then(data => renderAlbumInfo(data, albumInfo));
