12eae8f6d3c69e9ce75e29f516ac39dc43f12351
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-08-28 22:40:26 -0300

    SearchTrigger method

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
 
-const albums = spotify.search.albums('The Beatles');
-const albumList = document.getElementById('album-list');
+searchEnterTrigger();
 
 const album = spotify.album.getAlbum('7vEJAtP3KgKSpOHVgwm3Eh');
 const albumInfo = document.getElementById('album-info');
 
 const albumTracks = document.getElementById('album-tracks');
 
-albums.then(data => renderAlbums(data.albums.items, albumList));
 album.then(data => renderAlbumInfo(data, albumInfo))
   .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
